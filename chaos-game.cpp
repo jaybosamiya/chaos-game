5c55486c74a322e81c311b2c7937fb4092019157
Jay Bosamiya <jaybosamiya@gmail.com>
2016-02-03 17:32:41 +0530

    Add chaos game player

--- chaos-game.cpp
+++ chaos-game.cpp
@@ -1,38 +1,121 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 
 using namespace cv;
 using namespace std;
 
-void run_filter(Mat input_image, Mat &output_image) {
-  // TODO: Rewrite this as necessary for the filter
-  // Currently all it does is just output same image
-  output_image = input_image;
+const char * window_name = "Chaos game (c) Jay Bosamiya";
+const int max_iterations = (int)1e5;
+const int display_every = (int)1e2;
+
+vector<Point> point_set;
+int r100 = 50;
+
+void display_fractal();
+void display_points();
+
+void delete_point() {
+  if ( point_set.size() >= 1 ) {
+    point_set.pop_back();
+  }
+  display_points();
 }
 
-void show_help(const char *progname) {
-    cerr <<"Usage: " << progname << " input_image output_image" << endl;
+void waiter(int delay = 0) {
+  char c = waitKey(delay);
+  switch (c) {
+    case 'f':
+      display_fractal();
+      break;
+    case 'q':
+      exit(0);
+      break;
+    case 'x':
+      delete_point();
+      break;
+  }
+}
+
+void create_fractal(Mat &img, Point p) {
+  if ( point_set.size() < 1 ) {
+    return;
+  }
+
+  RNG rng;
+  double r = r100 / 100.0;
+
+  for ( int i = 0 ; i < max_iterations ; i++ ) {
+    Point towards = point_set[rng.uniform(0, point_set.size())];
+    p = r*p + (1-r)*towards;
+
+    img.at<Vec3b>(p)[0] = 255;
+    img.at<Vec3b>(p)[1] = 255;
+    img.at<Vec3b>(p)[2] = 255;
+
+    if ( i % display_every == 0 ) {
+      imshow(window_name, img);
+      waiter(1);
+    }
+  }
+}
+
+void display_fractal() {
+  Mat display_image(600, 600, CV_8UC3, Scalar(0,0,0));
+
+  if ( point_set.size() < 1 ) {
+    return;
+  }
+
+  Point centre(0,0);
+  for ( vector<Point>::iterator it = point_set.begin() ; it != point_set.end() ; it++ ) {
+    centre += *it;
+  }
+  centre = centre * (1.0/point_set.size());
+
+  create_fractal(display_image, centre);
+
+  waiter();
+}
+
+void display_points() {
+  Mat display_image(600, 600, CV_8UC3, Scalar(0,0,0));
+
+  for ( vector<Point>::iterator it = point_set.begin() ; it != point_set.end() ; it++ ) {
+    circle(display_image, *it, 5, Scalar(255,255,255), -1, 8);
+  }
+  if ( point_set.size() >= 1 ) {
+    circle(display_image, *(point_set.rbegin()), 5, Scalar(0,0,255), -1, 8);
+  }
+
+  imshow(window_name, display_image);
+  waiter();
+}
+
+void on_mouse(int event, int x, int y, int, void*) {
+  if( event != EVENT_LBUTTONDOWN )
+    return;
+
+  point_set.push_back(Point(x, y));
+
+  display_points();
 }
 
 int main( int argc, char** argv ) {
-  if( argc < 3 ) {
-    show_help(argv[0]);
-    return -1;
-  }
+  namedWindow(window_name, CV_WINDOW_AUTOSIZE);
 
-  Mat input_image, output_image;
+  cout << "Usage:\n"
+          "\tLeft Click: Add new point\n"
+          "\tKeypress x: Delete latest point\n"
+          "\tKeypress f: Run the chaos game\n"
+          "\tKeypress q: Quit\n";
 
-  input_image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  if ( ! input_image.data ) {
-      show_help(argv[0]);
-      cerr <<  "ERROR: Could not open or find the image " << argv[1] << endl;
-      return -1;
-  }
-
-  run_filter(input_image, output_image);
-
-  imwrite(argv[2],output_image);
+  setMouseCallback(window_name, on_mouse);
+  createTrackbar("r*100: ", window_name, &r100, 100, NULL);
+  display_points();
 
   return 0;
 }
